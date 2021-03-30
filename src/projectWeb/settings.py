aee2ef1b3bfa35556431e4772cd686c7fa2b80aa
Bao <totti.chelsea@gmail.com>
2021-03-30 10:07:49 +0700

    send video

--- src/projectWeb/settings.py
+++ src/projectWeb/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = "f%#wz-o!prt4oj%atus&52+-az&qcb-z(3ynh0l37rhp^@df8e"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["10.10.32.119", "192.168.123.147"]
+ALLOWED_HOSTS = ["10.10.36.35", "192.168.123.147"]
 
 
 # Application definition
