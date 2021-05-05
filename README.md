9a4561831a6b2b11aa49d9071fb5ea14aa5a1d56
Bao <totti.chelsea@gmail.com>
2021-05-05 09:59:01 +0700

    ...

--- README.md
+++
@@ -1,1 +0,0 @@
-Website Su Dung Django 
--- src/realtime/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.drivesMangementPage, name="drives"),
-    path("drives_of_pi/<str:id>/", views.drivesOfPi, name="drivesOfPi"),
-    path("evidence/", views.drivesOfPi, name="image"),
-]
