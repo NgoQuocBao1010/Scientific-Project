b1d0fe45549addc985e5d340a6edb625941a251a
Bao <totti.chelsea@gmail.com>
2021-05-05 11:18:15 +0700

    The end

---
+++ src/accounts/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    ROLES = (
+        ("admin", "admin"),
+        ("driver", "driver"),
+    )
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    role = models.CharField(max_length=20, choices=ROLES)
+    email = models.CharField(max_length=30, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    profilePic = models.ImageField(default="huongtram.png", null=True, blank=True)
+
+    def __str__(self):
+        return self.name + " profile"
+
+
+class Driver(models.Model):
+    profile = models.OneToOneField(Profile, null=True, on_delete=models.CASCADE)
+    licenseDriver = models.CharField(max_length=10, null=True, blank=True)
+
+    def __str__(self):
+        return "Driver " + self.profile.name
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=50)
+    driver = models.ForeignKey(Driver, null=True, on_delete=models.CASCADE)
+    licensePlate = models.CharField(max_length=10, null=True)
+    boughtDate = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class RaspDevice(models.Model):
+    STATUS = (
+        ("online", "online"),
+        ("offline", "offline"),
+    )
+    name = models.CharField(max_length=50)
+    car = models.OneToOneField(Car, null=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=50, null=True, blank=True, choices=STATUS)
+    lastActive = models.DateTimeField(null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
