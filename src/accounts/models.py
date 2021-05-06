cd40d20bfb3e719a2da5fc7e13803b17ae41b476
Bao <totti.chelsea@gmail.com>
2021-05-06 11:29:38 +0700

    html

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return "Company " + self.name
+
+
 class Profile(models.Model):
     ROLES = (
         ("admin", "admin"),
