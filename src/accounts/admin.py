f695d43c05781dfc892e285a2e2d7116b094ced1
NgoQuocBao1010 <56999031+NgoQuocBao1010@users.noreply.github.com>
2021-05-14 13:28:55 +0700

    notifications kinda complete

--- src/accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-admin.site.register(Company)
-admin.site.register(Profile)
-admin.site.register(Driver)
-admin.site.register(Car)
-admin.site.register(RaspDevice)
--- src/realtime/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("<str:id>", views.detail, name="detail"),
-]
